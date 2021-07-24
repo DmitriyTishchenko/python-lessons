47956c30be9d2ccc86db1b0f977d24e2c9640a1f
DmitriyTishchenko <kt041084tdn@gmail.com>
2021-07-24 20:46:23 +0300

    Lesson 6. Variables

---
+++ lesson6.py
@@ -0,0 +1,18 @@
+x = 1          # INTEGER
+y = 3.5        # FLOAT
+my_var1 = 50
+_myvar1 = 100
+
+myVar = 30
+
+print(type(x))
+print(type(y))
+
+X = 2
+x = 15
+print(x, X)
+
+TEST = 20
+print(TEST)
+TEST = 40
+print(TEST)
