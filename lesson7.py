41659ccb9bbe5c03ce5fe2765c2e733100ecfbe7
DmitriyTishchenko <kt041084tdn@gmail.com>
2021-07-26 00:02:46 +0300

    Lesson 7. Boolean Data Type

---
+++ lesson7.py
@@ -0,0 +1,22 @@
+test = None
+test = 123
+print(test)
+
+x, y = (3, 5)
+print(x, y)
+
+# boolean
+my_true = True
+my_false = False
+print(my_false, my_true)
+
+# str(), int(), bool(), float()
+x = 5.2
+print(x, type(x))
+x = int(x)
+print(x, type(x))
+
+x = 0
+print(x, type(x))
+x = bool(x)
+print(x, type(x))
